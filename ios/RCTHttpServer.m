7ffbe6e630eaaac0dc638fce3c0bd16e64c38b49
arturzinin <a.zinin@goit.ua>
2023-03-14 18:16:52 +0200

    fix: OS EXC_BAD_ACCESS

--- ios/RCTHttpServer.m
+++ ios/RCTHttpServer.m
@@ -1,3 +1,4 @@
+
 #import "RCTHttpServer.h"
 #import "React/RCTBridge.h"
 #import "React/RCTLog.h"
@@ -28,12 +29,11 @@ RCT_EXPORT_MODULE();
     [server addDefaultHandlerForMethod:type
                           requestClass:[GCDWebServerDataRequest class]
                      asyncProcessBlock:^(GCDWebServerRequest* request, GCDWebServerCompletionBlock completionBlock) {
-        
+
         long long milliseconds = (long long)([[NSDate date] timeIntervalSince1970] * 1000.0);
         int r = arc4random_uniform(1000000);
         NSString *requestId = [NSString stringWithFormat:@"%lld:%d", milliseconds, r];
 
-        _completionBlocks = [[NSMutableDictionary alloc] init];
          @synchronized (self) {
              [_completionBlocks setObject:completionBlock forKey:requestId];
          }
@@ -65,16 +65,17 @@ RCT_EXPORT_METHOD(start:(NSInteger) port
                   serviceName:(NSString *) serviceName)
 {
     RCTLogInfo(@"Running HTTP bridge server: %ld", port);
-    NSMutableDictionary *_requestResponses = [[NSMutableDictionary alloc] init];
-    
+
+    _completionBlocks = [[NSMutableDictionary alloc] init];
+
     dispatch_sync(dispatch_get_main_queue(), ^{
         _webServer = [[GCDWebServer alloc] init];
-        
+
         [self initResponseReceivedFor:_webServer forType:@"POST"];
         [self initResponseReceivedFor:_webServer forType:@"PUT"];
         [self initResponseReceivedFor:_webServer forType:@"GET"];
         [self initResponseReceivedFor:_webServer forType:@"DELETE"];
-        
+
         [_webServer startWithPort:port bonjourName:serviceName];
     });
 }
@@ -82,7 +83,7 @@ RCT_EXPORT_METHOD(start:(NSInteger) port
 RCT_EXPORT_METHOD(stop)
 {
     RCTLogInfo(@"Stopping HTTP bridge server");
-    
+
     if (_webServer != nil) {
         [_webServer stop];
         [_webServer removeAllHandlers];
@@ -95,7 +96,6 @@ RCT_EXPORT_METHOD(respond: (NSString *) requestId
                   type: (NSString *) type
                   body: (NSString *) body)
 {
-    NSData* data = [body dataUsingEncoding:NSUTF8StringEncoding];
     GCDWebServerDataResponse* requestResponse = [[GCDWebServerDataResponse alloc] initWithData:data contentType:type];
     requestResponse.statusCode = code;
 
@@ -104,8 +104,7 @@ RCT_EXPORT_METHOD(respond: (NSString *) requestId
         completionBlock = [_completionBlocks objectForKey:requestId];
         [_completionBlocks removeObjectForKey:requestId];
     }
-
-    completionBlock(requestResponse);
+        completionBlock(requestResponse);
 }
 
 @end
