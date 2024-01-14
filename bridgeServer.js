6b1ed1222e80af59c0513414e491f60998502a20
Alwin J. Schuster <contact@alwinschuster.at>
2024-01-14 17:13:59 +0100

    fix: Add support for GET/URL variables

--- bridgeServer.js
+++ bridgeServer.js
@@ -88,7 +88,7 @@ class BridgeServer {
       const callbacks = this.callbacks.filter(
         c =>
           (c.method === request.type || c.method === '*') &&
-          (c.url === request.url || c.url === '*'),
+          (c.url === request.url.split("?")[0] || c.url === '*'),
       );
 
       for (const c of callbacks) {
